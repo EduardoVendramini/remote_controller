24fe1f1e795f80a0864a547449e2a82fa1da934a
EduardoVendramini <85597315+EduardoVendramini@users.noreply.github.com>
2024-10-27 19:12:39 -0300

    Prints bluetooth input on serial and on lcd display

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This is a project for the university and the purpose is to control two brushless
 
 ## remote_control
 This code is made to send data to the PID controller via bluetooth. The remote control has a lcd display for the user to set different parameters of the controller.
+
+
+## Setup
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,51 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <BluetoothSerial.h>
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+const char *pin = "1234";
+String slaveName = "HC-06-MODULO5";
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+String incomingMessage = "";
 
 void setup()
 {
   Serial.begin(9600);
+
+  SerialBT.begin("ESP32", true);
+  SerialBT.setPin(pin);
+
+  //while (!SerialBT.connect(slaveName))
+  //{
+  //  Serial.println("Trying to connect to" + slaveName);
+  //}
+  //Serial.println("Bluetooth connected");
+
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("Olaaaa!!!!!");
-  Serial.println("Olaaaa!!!!!");
+  lcd.print("Waiting input...");
 }
 
 void loop()
 {
+  if (SerialBT.available())
+  {
+    incomingMessage = SerialBT.readString();
+
+    Serial.println(incomingMessage);
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(incomingMessage);
+
+    SerialBT.println(incomingMessage);
+  }
 }
