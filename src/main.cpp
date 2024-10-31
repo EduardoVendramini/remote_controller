6edd92aeda567455432c5746235d8a7bc69ebf41
EduardoVendramini <85597315+EduardoVendramini@users.noreply.github.com>
2024-10-31 15:25:43 -0300

    Bluetooth communication is working. It reads the Serial input and sends it to the bluetooth serial.

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,32 @@
 #include <LiquidCrystal_I2C.h>
 #include <BluetoothSerial.h>
 
+#define BT_DISCOVER_TIME 10000
+
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
-
-BluetoothSerial SerialBT;
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 String incomingMessage = "";
 
+BluetoothSerial SerialBT;
+uint8_t address[6] = {0x98, 0xDA, 0x60, 0x00, 0xEB, 0x2E}; // MAC Address of the slave, got from scanning
+void bluetoothScan();
+void bluetoothConnect(uint8_t address[]);
+
 void setup()
 {
+  // Serial
   Serial.begin(9600);
 
+  // Bluetooth
   SerialBT.begin("ESP32", true);
+  // bluetoothScan();
+  bluetoothConnect(address);
 
+  // LCD
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
@@ -27,16 +37,44 @@ void setup()
 
 void loop()
 {
-  if (SerialBT.available())
+  if (Serial.available())
   {
-    incomingMessage = SerialBT.readString();
+    incomingMessage = Serial.readString();
 
     Serial.println(incomingMessage);
+    SerialBT.println(incomingMessage);
 
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(incomingMessage);
 
-    SerialBT.println(incomingMessage);
+    incomingMessage = "";
+  }
+}
+
+void bluetoothScan()
+{
+  Serial.println("Starting discover...");
+  BTScanResults *pResults = SerialBT.discover(BT_DISCOVER_TIME);
+  if (pResults)
+    pResults->dump(&Serial);
+  else
+    Serial.println("Error on BT Scan, no result!");
+}
+
+void bluetoothConnect(uint8_t address[])
+{
+  for (int i = 0; i < 3; i++)
+  {
+    Serial.println("Connecting...");
+    if (SerialBT.connect(address))
+    {
+      Serial.println("Connected");
+      return;
+    }
+    else
+    {
+      Serial.println("Failed to connect");
+    }
   }
 }
